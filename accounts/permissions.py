06a40e058a9f20c5136c50f4a9c2511ca7a93889
Omar Ahmed Mohammed Abdel Maksoud <132158344+OmarMaksoud@users.noreply.github.com>
2024-05-07 14:53:08 +0300

    added customers and store permessions

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -13,3 +13,33 @@ class IsNotAuthenticated(permissions.BasePermission):
             return True
 
         return bool(not request.user.is_authenticated)
+
+
+class IsNotAuthenticatedOrIsCustomer(permissions.BasePermission):
+    """
+    Custom permission to allow access to unauthenticated users or customers
+    """
+
+    def has_permission(self, request, view):
+        return not request.user.is_authenticated or (
+            request.user.groups.filter(name="Customers").exists()
+            and not request.user.groups.filter(name="Stores").exists()
+        )
+
+
+class IsStore(permissions.BasePermission):
+    """
+    Custom permission to only allow sellers to access the view
+    """
+
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="Stores").exists()
+
+
+class IsCustomer(permissions.BasePermission):
+    """
+    Custom permission to only allow buyers to access the view
+    """
+
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="Customers").exists()
